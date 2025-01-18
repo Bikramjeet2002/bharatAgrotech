ccbb3d4f9ad6d0caa77bdc6da44d26151c338512
bikram <bikram@gmail.com>
2025-01-18 20:00:33 +0530

    backend_added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://bharatagrotech-backend.onrender.com";
 
 export const navigation = [
   { name: "Home ", href: "/" },
